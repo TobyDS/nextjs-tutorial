bf8dbcf3c78fc925252adeb368bb5cea6c7fae4d
TobyDS <tobydixonsmith@gmail.com>
2024-05-09 12:58:46 +0100

    feat: add Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,19 @@
+import { Metadata } from 'next';
+
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
- 
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL(
+    'https://nextjs-tutorial-p3i5d73jx-toby-dixon-smiths-projects.vercel.app/'
+  ),
+};
+
 export default function RootLayout({
   children,
 }: {
